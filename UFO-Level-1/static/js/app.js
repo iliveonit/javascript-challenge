3d35fe8a6ea41976620ca3a27510a0942ee338f1
Aslam Momin <iliveonit@gmail.com>
2020-06-06 18:51:13 -0400

    app.js: tbody-tr visible

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+// // Console.log the weather data from data.js
+// console.log(tableData);
+
+// // Step 1: Loop Through `tableData` and console.log each UFO report object
+// data.forEach(function(tableData) {
+//   console.log(tableData);
+// });
+
+// // Step 2:  Use d3 to append one table row `tr` for each UFO report object
+// data.forEach(function(tableData) {
+//   console.log(tableData);
+//   var row = tbody.append("tr");
+// });
+
+// Step 3:  Use `Object.entries` to console.log each URO report value
+data.forEach(function(tableData) {
+  console.log(tableData);
+  var row = tbody.append("tr");
+  Object.entries(tableData).forEach(function([key, value]) {
+    console.log(key, value);
+  });
+});
