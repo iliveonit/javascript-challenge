aa35691d7d1cdffe037b3ff6e4e4d833e130f6ed
Aslam Momin <iliveonit@gmail.com>
2020-06-08 00:11:56 -0400

    Filter-Date button added; doesn't work yet

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -8,6 +8,10 @@ var tbody = d3.select("tbody");
 // Getting a reference to the button on the page with the id property set to `click-me`
 var button = d3.select("#Enter");
 
+
+// =======================================================================================
+// Part-1 : Create and Load data on pressing 'Enter'
+
 // Getting a reference to the input element on the page with the id property set to 'input-field'
 var inputField = d3.select("#Enter");
 
@@ -25,6 +29,52 @@ function enterClick() {
 
 // We can use the `on` function in d3 to attach an event to the handler function
 button.on("click", enterClick);
+
+// =======================================================================================
+// Part-2 : Filter data based on date input
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runFilter);
+form.on("submit",runFilter);
+
+// Complete the event handler function for the form
+function runFilter() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(sighting => sighting.date === inputValue);
+
+
+  // This function is filters the date-input to display results
+
+function runFilter() {
+  // console.log("Hi, Welcome !!!");
+      data.forEach((tableData) => {
+       var row = tbody.append("tr");
+        Object.entries(tableData).forEach(([key, value]) => {
+       var cell = row.append("td");
+        cell.text(value);
+       });
+      });
+}
+
+  console.log(filteredData);
+
+}
+// =======================================================================================
 
 // You can also define the click handler inline
 // button.on("Enter", function msg() {
